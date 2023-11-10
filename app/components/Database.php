dee8dbf7c4222055be9f902ab0915b327dc4059f
AlexFilonenko88 <af88_88@mail.ru>
2023-11-10 21:19:23 +0300

    add files project

---
+++ app/components/Database.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\components;
+
+use PDO;
+
+
+class Database
+{
+    public static function connect () : PDO
+    {
+
+    }
+}
--- index.php
+++ index.php
@@ -1,1 +1,33 @@
 <?php
+require_once __DIR__ . "/vendor/autoload.php";
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Магазин книг</h1>
+    </header>
+
+    <section>
+        <div class="container">
+            <div class="products">
+
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        Все права защищены
+    </footer>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
